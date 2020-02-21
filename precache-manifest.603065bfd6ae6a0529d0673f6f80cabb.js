7f9cc939bc59201d42e908322b01f524e0f9e351
angkasa27 <mas.angkasa27@gmail.com>
2020-02-21 09:13:26 +0800

    Updates

---
+++ precache-manifest.603065bfd6ae6a0529d0673f6f80cabb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5fea53cbe1e163df1f6adb0922abb5d",
+    "url": "/BootstrapGenerator/index.html"
+  },
+  {
+    "revision": "943e94aa4e3f272157e7",
+    "url": "/BootstrapGenerator/static/css/2.2ec3db0d.chunk.css"
+  },
+  {
+    "revision": "b17ba71b8816f4dd3bde",
+    "url": "/BootstrapGenerator/static/css/main.f78922aa.chunk.css"
+  },
+  {
+    "revision": "943e94aa4e3f272157e7",
+    "url": "/BootstrapGenerator/static/js/2.087565dd.chunk.js"
+  },
+  {
+    "revision": "d6fe596968784eded985a52b5dc6cf26",
+    "url": "/BootstrapGenerator/static/js/2.087565dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b17ba71b8816f4dd3bde",
+    "url": "/BootstrapGenerator/static/js/main.9d487079.chunk.js"
+  },
+  {
+    "revision": "761b100d4cc2f11067f3",
+    "url": "/BootstrapGenerator/static/js/runtime-main.7f2a4cef.js"
+  }
+]);
