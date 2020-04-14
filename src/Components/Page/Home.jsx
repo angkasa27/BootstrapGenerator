0643df23ecac8dc0e691f3ef6a7a95840abb0e4c
angkasa27 <mas.angkasa27@gmail.com>
2020-04-14 20:59:21 +0800

    Fix main page responsive

--- src/Components/Page/Home.jsx
+++ src/Components/Page/Home.jsx
@@ -17,18 +17,35 @@ class Home extends Component {
           <div className="row my-5">
             <div className="col-md-5">
               <h1 className="text-light">How to use it?</h1>
-              <p className="text-light ml-5">Create new HTML file<br />Copy The code</p>
+              <ul className="text-light ml-5">
+                <li>
+                  Create new HTML file
+                </li>
+                <li>
+                  Import Bootstrap
+                </li>
+                <li>
+                  Chose your style
+                </li>
+                <li>
+                  Copy The code!
+                </li>
+              </ul>
             </div>
             <div className="col-md-7">
               <Code />
             </div>
           </div>
 
+          <div className="row justify-content-center">
+            <h1 className="text-light mt-2">Component Generator</h1>
+          </div>
+
           <div className="row justify-content-md-center">
 
             {/* <div className="col-md-2"></div> */}
 
-            <div className="col-md-4">
+            <div className="col-md-4 mt-3">
               <div className="card rounded-lg shadow border-0 bg-warning ">
                 <h3 className="card-title bold text-center text-dark mt-3">Alert Generator</h3>
                 <div className="card-body">
@@ -36,8 +53,6 @@ class Home extends Component {
                     <li >Customize text color</li>
                     <li className="mt-2">Customize alert color</li>
                     <li className="mt-2">Customize text align</li>
-                    <li className="mt-2">Live code</li>
-                    <li className="mt-2">Output code</li>
                   </ul>
                   <div className="text-center mt-4">
                     <Link to="/AlertGenerator" className="btn btn-dark rounded-pill">
@@ -48,16 +63,14 @@ class Home extends Component {
               </div>
             </div>
 
-            <div className="col-md-4">
+            <div className="col-md-4 mt-3">
               <div className="card rounded-lg shadow border-0 bg-warning ">
                 <h3 className="card-title bold text-center text-dark mt-3">Button Generator</h3>
                 <div className="card-body">
                   <ul className="text-dark">
-                    <li >Customize text color</li>
-                    <li className="mt-2">Customize alert color</li>
-                    <li className="mt-2">Customize text align</li>
-                    <li className="mt-2">Live code</li>
-                    <li className="mt-2">Output code</li>
+                    <li>Customize button shape</li>
+                    <li className="mt-2">Customize button size </li>
+                    <li className="mt-2">Customize button color</li>
                   </ul>
                   <div className="text-center mt-4">
                     <Link to="/ButtonGenerator" className="btn btn-dark rounded-pill">
@@ -68,16 +81,72 @@ class Home extends Component {
               </div>
             </div>
 
-            <div className="col-md-4">
+            <div className="col-md-4 mt-3">
               <div className="card rounded-lg shadow border-0 bg-warning ">
                 <h3 className="card-title bold text-center text-dark mt-3">Card Generator</h3>
+                <div className="card-body">
+                  <ul className="text-dark">
+                    <li>Customize card type</li>
+                    <li className="mt-2">Customize card color </li>
+                    <li className="mt-2">Customize text size</li>
+                    <li className="mt-2">Customize text align</li>
+                    <li className="mt-2">Customize text position</li>
+                  </ul>
+                  <div className="text-center mt-4">
+                    <Link to="/CardGenerator" className="btn btn-dark rounded-pill">
+                      Open
+                  </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4 mt-3">
+              <div className="card rounded-lg shadow border-0 bg-warning ">
+                <h3 className="card-title bold text-center text-dark mt-3">Alert Generator</h3>
                 <div className="card-body">
                   <ul className="text-dark">
                     <li >Customize text color</li>
                     <li className="mt-2">Customize alert color</li>
                     <li className="mt-2">Customize text align</li>
-                    <li className="mt-2">Live code</li>
-                    <li className="mt-2">Output code</li>
+                  </ul>
+                  <div className="text-center mt-4">
+                    <Link to="/AlertGenerator" className="btn btn-dark rounded-pill">
+                      Open
+                  </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4 mt-3">
+              <div className="card rounded-lg shadow border-0 bg-warning ">
+                <h3 className="card-title bold text-center text-dark mt-3">Button Generator</h3>
+                <div className="card-body">
+                  <ul className="text-dark">
+                    <li>Customize button shape</li>
+                    <li className="mt-2">Customize button size </li>
+                    <li className="mt-2">Customize button color</li>
+                  </ul>
+                  <div className="text-center mt-4">
+                    <Link to="/ButtonGenerator" className="btn btn-dark rounded-pill">
+                      Open
+                  </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4 mt-3">
+              <div className="card rounded-lg shadow border-0 bg-warning ">
+                <h3 className="card-title bold text-center text-dark mt-3">Card Generator</h3>
+                <div className="card-body">
+                  <ul className="text-dark">
+                    <li>Customize card type</li>
+                    <li className="mt-2">Customize card color </li>
+                    <li className="mt-2">Customize text size</li>
+                    <li className="mt-2">Customize text align</li>
+                    <li className="mt-2">Customize text position</li>
                   </ul>
                   <div className="text-center mt-4">
                     <Link to="/CardGenerator" className="btn btn-dark rounded-pill">
